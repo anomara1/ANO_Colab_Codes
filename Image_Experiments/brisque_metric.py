459f4379cbdfab237044d7ac40828993a60f154a
ANOMARA <106452837+anomara1@users.noreply.github.com>
2025-03-04 10:06:03 +0200

    Update brisque_metric.py

--- Image_Experiments/brisque_metric.py
+++ Image_Experiments/brisque_metric.py
@@ -1,4 +1,13 @@
-!pip install image-quality
+import subprocess
+import sys
+
+def install_package(package):
+    """Function to install a Python package using pip"""
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Example usage
+install_package("image-quality")  # This will install the numpy package
+
 from imquality import brisque
 
 
